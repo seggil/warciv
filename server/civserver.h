80519aadd76fbb869e56b3e908d05812b1ae615d
pepeto <pepeto@4efe6140-7f1d-0410-9bd2-be1ee28408da>
2007-05-14 16:21:27 +0000

    (no comment)
    git-svn-id: svn://svn.gna.org/svn/freeciv-warclient/trunk@317 4efe6140-7f1d-0410-9bd2-be1ee28408da

--- server/civserver.h
+++ server/civserver.h
@@ -13,6 +13,6 @@
 #ifndef FC__CIVSERVER_H
 #define FC__CIVSERVER_H
 
-#define PEPSERVER_VERSION "Warserver - PepServer 0.9.2 devel"
+#define PEPSERVER_VERSION "Warserver - PepServer 0.9.3 devel"
 
 #endif /* FC__CIVSERVER_H */
