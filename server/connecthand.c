ce8f737d8523e1b2de6dafb1a9cb89a4b9b0dbf5
pepeto <pepeto@4efe6140-7f1d-0410-9bd2-be1ee28408da>
2007-06-12 10:58:04 +0000

    + fix maxplayer bug
    git-svn-id: svn://svn.gna.org/svn/freeciv-warclient/trunk@373 4efe6140-7f1d-0410-9bd2-be1ee28408da

--- server/connecthand.c
+++ server/connecthand.c
@@ -656,8 +656,8 @@ bool attach_connection_to_player(struct connection *pconn,
 {
   /* if pplayer is NULL, attach to first non-connected player slot */
   if (!pplayer) {
-    if (game.nplayers > game.max_players
-	|| game.nplayers > MAX_NUM_PLAYERS + MAX_NUM_BARBARIANS
+    if (game.nplayers >= game.max_players
+	|| game.nplayers >= MAX_NUM_PLAYERS + MAX_NUM_BARBARIANS
 	|| !can_control_a_player(pconn, TRUE) ) {
       return FALSE;
     } else {
