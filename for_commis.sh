77d99c9f76a612e4be01983e2ba98cb1a27be469
woodswolf <woodswolf@4efe6140-7f1d-0410-9bd2-be1ee28408da>
2007-10-02 12:20:13 +0000

    + improve script : take in account new modified file from freeciv(already existant)
    git-svn-id: svn://svn.gna.org/svn/freeciv-warclient/trunk@702 4efe6140-7f1d-0410-9bd2-be1ee28408da

--- for_commis.sh
+++ for_commis.sh
@@ -21,15 +21,15 @@ else
 fi
 
 if [ "$2" == '' ]; then
-    TMPDIR=warclient.new
+    MY_TMPDIR=warclient.new
 else
-    TMPDIR=$2
+    MY_TMPDIR=$2
 fi
 
 #check if merged directory is avaliable
-if [ -a $TMPDIR ]; then
-    echo Directory $TMPDIR exist already
-    echo "First delete it ( rm -fr $TMPDIR ) or"
+if [ -a $MY_TMPDIR ]; then
+    echo Directory $MY_TMPDIR exist already
+    echo "First delete it ( rm -fr $MY_TMPDIR ) or"
     echo "Change name of <output_patch_file>"
     exit 1
 fi
@@ -52,45 +52,79 @@ fi
 DIRWAR=warclient
 DIRFREE=freeciv-2.0
 
-cp -a $DIRWAR $TMPDIR
+cp -a $DIRWAR $MY_TMPDIR
 #verify if PATH absolute or relative is given
-cd $TMPDIR/..
+cd $MY_TMPDIR/..
 if [ "$pwd" != `pwd` ]; then
     echo "<your_tmp_dir> must not contain and / ; it must be create in this directory"
     cd $pwd
-    rm -fr $TMPDIR
+    rm -fr $MY_TMPDIR
     exit
 fi
 cd $pwd
 
 for fichier in `find $DIRWAR ! -wholename '*/.svn*'  `; do
-
-echo $fichier | grep -v '/$' | sed 's|[^/]*||' | while read -r name
-  do
-if [ ! -d $fichier ]; then
+    
+    echo $fichier | grep -v '/$' | sed 's|[^/]*||' | while read -r name
+    do
+	if [ ! -d $fichier ]; then
 #  echo $fichier $WORKDIR/$name
-    TEST=`diff $fichier $WORKDIR/$name`
+	    TEST=`diff $fichier $WORKDIR/$name`
 #echo $TEST
-    if [ "$TEST" != '' ]; then
-	cp $WORKDIR/$name $TMPDIR/$name
-	echo "diff -urNad $DIRWAR/$name $TMPDIR/$name"  >>  $patch
-	diff -urNad $DIRWAR/$name $TMPDIR/$name >>  $patch
-	echo "# Modified : $name"
-    fi
+	    if [ "$TEST" != '' ]; then
+		cp $WORKDIR/$name $MY_TMPDIR/$name
+		echo "diff -urNad $DIRWAR/$name $MY_TMPDIR/$name"  >>  $patch
+		diff -urNad $DIRWAR/$name $MY_TMPDIR/$name >>  $patch
+		echo "# Modified : $name"
+	    fi
+	fi
+	
+    done
+
+done
+    
+DATE=`date +%s`
+if [ ! -f $MY_TMPDIR/tmp$DATE ]; then
+    touch  $MY_TMPDIR/tmp$DATE
+else
+    echo "Hmm, $MY_TMPDIR/tmp$DATE exist! How is it possible?"
+    echo "Retry and if don't work, we found a bug"
+    exit 1
 fi
 
+for fichier in `find $DIRFREE ! -wholename '*/.svn*'  `; do
+    
+    echo $fichier | grep -v '/$' | sed 's|[^/]*||' | while read -r name
+    do
+	if [ ! -d $fichier ] && [ -e $WORKDIR/$name ]; then
+#  echo $fichier $WORKDIR/$name
+	    TEST=`diff $fichier $WORKDIR/$name`
+#echo $TEST
+	    if [ "$TEST" != '' ] && [ ! -e $DIRWAR/$name ] ; then
+		echo "# Modified and Added to warclient: $name"
+		echo "# Modified and Added to warclient: $name" >> $MY_TMPDIR/tmp$DATE
+		echo -e "cp -a $WORKDIR/$name $DIRWAR/$name" >> $MY_TMPDIR/tmp$DATE
+		echo -e "svn add $DIRWAR/$name" >> $MY_TMPDIR/tmp$DATE
+	    fi
+	fi
+    done
 done
 
-done
-
-echo "*****************************************************************"
-echo "*                    WARNING                                    *"
-echo "*****************************************************************"
-echo "if you have modified a file witch is not already in warclient dir"
-echo "First copy it in warclient dir, perform svn add <file>"
-echo -e "Must be fixed \n \n"
-echo "Now run the following commands"
-echo "cd $DIRWAR"
-echo "patch -p1 < $patch"
-echo -e "\n \nNow do your commis"
-echo "svn ci -m \"Your job\""
+echo "*******************************************************************"
+echo "*                      WARNING                                    *"
+echo "*******************************************************************"
+echo -e "If you have added a new file, with a name non existant"
+echo -e "into warclient or freeciv dir copy it in warclient dir,"
+echo -e "and perform : svn add <file>"
+echo "*******************************************************************"
+echo -e "\n All modified files will be introduce in tree"
+echo -e "       by running the following commands : \n"
+echo -e "cd $DIRWAR"
+echo -e "patch -p1 < $patch"
+if [ "`more $MY_TMPDIR/tmp$DATE`" != '' ]; then
+echo -e "cd .."
+echo -e "#You must add theses files into svn repository ; Run"
+fi
+cat $MY_TMPDIR/tmp$DATE
+echo -e "\n\n#Now do your commis"
+echo -e "svn ci -m \"Your job\""
