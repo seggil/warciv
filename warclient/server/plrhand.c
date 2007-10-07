879776a455e70eaaf4dc761b015f0b73a30d6c0f
pepeto <pepeto@4efe6140-7f1d-0410-9bd2-be1ee28408da>
2007-10-07 10:05:33 +0000

    + initilialize research structures.
    git-svn-id: svn://svn.gna.org/svn/freeciv-warclient/trunk@734 4efe6140-7f1d-0410-9bd2-be1ee28408da

--- warclient/server/plrhand.c
+++ warclient/server/plrhand.c
@@ -703,7 +703,11 @@ void init_tech(struct player *plr)
   } tech_type_iterate_end;
   set_invention(plr, A_NONE, TECH_KNOWN);
 
+  plr->research.bulbs_last_turn = 0;
+  plr->research.bulbs_researched = 0;
   plr->research.techs_researched = 1;
+  plr->research.changed_from = A_NONE;
+  plr->research.bulbs_researched_before = 0;
 
   /* Mark the reachable techs */
   update_research(plr);
