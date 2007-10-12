f73d9cc3558fcd50f89459a6089262bfb48f6350
woodswolf <woodswolf@4efe6140-7f1d-0410-9bd2-be1ee28408da>
2007-10-12 09:25:18 +0000

    + Fix compilation problem specific to win32 ocuring after an update of Msys software : some type need to be defined. Why? ask to Bill :)
    git-svn-id: svn://svn.gna.org/svn/freeciv-warclient/trunk@753 4efe6140-7f1d-0410-9bd2-be1ee28408da

--- warclient/utility/tadns.c
+++ warclient/utility/tadns.c
@@ -58,9 +58,9 @@
 #pragma comment(lib,"advapi32")
 #endif
 typedef int socklen_t;
-// typedef unsigned char uint8_t;
-// typedef unsigned short uint16_t;
-//typedef unsigned int uint32_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
 #endif /* WIN32_NATIVE */
 
 
