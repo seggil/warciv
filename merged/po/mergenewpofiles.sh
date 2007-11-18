874f9d1196b204c7ad9677ef78ad1ac008867e0e
pepeto <pepeto@4efe6140-7f1d-0410-9bd2-be1ee28408da>
2007-11-18 18:21:54 +0000

    + remove useless files.
    git-svn-id: svn://svn.gna.org/svn/freeciv-warclient/trunk@800 4efe6140-7f1d-0410-9bd2-be1ee28408da

--- merged/po/mergenewpofiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for fichier in `ls *.po`; do
-    echo $fichier
-    msgmerge --compendium=$fichier ../../freeciv-2.0/po/$fichier freeciv.pot -o ${fichier}x
-    mv ${fichier}x $fichier
-done
