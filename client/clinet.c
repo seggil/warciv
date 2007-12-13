cde83373fdb457eb26f7feed37e16a653571f9ba
woodswolf <woodswolf@4efe6140-7f1d-0410-9bd2-be1ee28408da>
2007-12-13 13:03:58 +0000

    + oops ifndef not ifdef
    git-svn-id: svn://svn.gna.org/svn/freeciv-warclient/trunk@867 4efe6140-7f1d-0410-9bd2-be1ee28408da

--- client/clinet.c
+++ client/clinet.c
@@ -1126,7 +1126,7 @@ metaserver_name_lookup_callback(union my_sockaddr *addr_result, void *data)
   }
   ctx->sock = sock;
 
-#ifdef WIN32_NATIVE
+#ifndef WIN32_NATIVE
 /*   if we have winsock, we must let's glib to make this  */
 /*   socket in  non-blocking mode to prevent busy wait socket*/
   if (-1 == my_nonblock(sock)) {
