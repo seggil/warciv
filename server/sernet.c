ff5cd6144bff1be67ccaf37468887cf182acbde8
pepeto <pepeto@4efe6140-7f1d-0410-9bd2-be1ee28408da>
2009-08-04 11:04:51 +0000

    + server_accept_connection() always accept the new connection before rejecting it.  It might have some unexcepted behaviour before.
    git-svn-id: svn://svn.gna.org/svn/freeciv-warclient/trunk@1653 4efe6140-7f1d-0410-9bd2-be1ee28408da

--- server/sernet.c
+++ server/sernet.c
@@ -1097,17 +1097,18 @@ static int server_accept_connection(int sockfd)
   freelog(LOG_DEBUG, "sac server_accept_connection sockfd=%d", sockfd);
   fromlen = sizeof(fromend);
 
+  if ((new_sock = accept(sockfd, &fromend.sockaddr, &fromlen)) == -1) {
+    freelog(LOG_ERROR, "accept failed: %s",
+            mystrsocketerror(mysocketerrno()));
+    return -1;
+  }
+
   if ((game.server.maxconnections > 0)
       && (conn_list_size(game.all_connections)
           > game.server.maxconnections)) {
     freelog(LOG_NORMAL, _("Maximum number of connections "
 			  "for this server exceeded."));
-    return -1;
-  }
-
-  if ((new_sock = accept(sockfd, &fromend.sockaddr, &fromlen)) == -1) {
-    freelog(LOG_ERROR, "accept failed: %s",
-            mystrsocketerror(mysocketerrno()));
+    my_closesocket(new_sock);
     return -1;
   }
 
