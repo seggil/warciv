ef31c8abd8e8a97cc79e9d96bdb1c20890802751
Gilles J. Seguin <segg.gill@gmail.com>
2011-08-18 17:59:59 -0400

    fcintl.h to wc_intl.h, error msg

--- utility/shared.h
+++ utility/shared.h
@@ -19,7 +19,7 @@
 
 #ifdef HAVE_CONFIG_H
 #  ifndef WC_CONFIG_H            /* this should be defined in config.h */
-#    error Files including fcintl.h should also include config.h directly
+#    error Files including wc_intl.h should also include config.h directly
 #  endif
 #endif
 
