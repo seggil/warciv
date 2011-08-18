491d903181cb8bd7676ffc66eaa9aaf9fedf6b47
Gilles J. Seguin <segg.gill@gmail.com>
2011-08-18 13:12:27 -0400

    utility, change <config.h> to "../config.h"

--- utility/sbuffer.c
+++ utility/sbuffer.c
@@ -31,7 +31,7 @@ sbuffer: ("string buffer")
 ***************************************************************************/
 
 #ifdef HAVE_CONFIG_H
-#include <config.h>
+#  include "../config.h"
 #endif
 
 #include <assert.h>
